3f8ff892f3b7132a4cd32fb9fa1da03dfa734b20
NRegret <dickyh18@gmail.com>
2023-02-21 06:12:57 +0700

    main nya nanti ae internet sini bapuk ga mud

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,10 +7,41 @@ class Character {
 private:
     static Character* instance;
     int x, y;
+    int spawnX, spawnY;
+    int finishX, finishY;
+    vector<pair<int, int>> path;
+    int pathIndex;
+    int score;
+
     Character() {
         x = 0;
         y = 0;
+        spawnX = 0;
+        spawnY = 0;
+        finishX = 10;
+        finishY = 10;
+        path = generatePath(spawnX, spawnY, finishX, finishY);
+        pathIndex = 0;
+        score = 0;
     } // constructor private
+
+    vector<pair<int, int>> generatePath(int startX, int startY, int endX, int endY) {
+        vector<pair<int, int>> path;
+        int dx = endX - startX;
+        int dy = endY - startY;
+        int steps = max(abs(dx), abs(dy));
+        float xIncrement = (float) dx / steps;
+        float yIncrement = (float) dy / steps;
+        float x = startX;
+        float y = startY;
+        for (int i = 0; i <= steps; i++) {
+            path.push_back(make_pair((int) x, (int) y));
+            x += xIncrement;
+            y += yIncrement;
+        }
+        return path;
+    }
+
 public:
     static Character* getInstance() {
         if (instance == nullptr) {
@@ -19,19 +51,51 @@ public:
     }
 
     void moveUp() {
-        y--;
+        if (y > 0) {
+            y--;
+            checkPath();
+        }
     }
 
     void moveDown() {
-        y++;
+        if (y < 19) {
+            y++;
+            checkPath();
+        }
     }
 
     void moveLeft() {
-        x--;
+        if (x > 0) {
+            x--;
+            checkPath();
+        }
     }
 
     void moveRight() {
-        x++;
+        if (x < 19) {
+            x++;
+            checkPath();
+        }
+    }
+
+    void setSpawnPoint(int x, int y) {
+        spawnX = x;
+        spawnY = y;
+        path = generatePath(spawnX, spawnY, finishX, finishY);
+        pathIndex = 0;
+        score = 0;
+    }
+
+    void setFinishPoint(int x, int y) {
+        finishX = x;
+        finishY = y;
+        path = generatePath(spawnX, spawnY, finishX, finishY);
+        pathIndex = 0;
+        score = 0;
+    }
+
+    bool hasReachedFinishPoint() {
+        return x == finishX && y == finishY;
     }
 
     int getX() {
@@ -40,6 +104,17 @@ public:
 
     int getY() {
         return y;
+    }
+
+    int getScore() {
+        return score;
+    }
+
+    void checkPath() {
+        if (pathIndex < path.size() && path[pathIndex].first == x && path[pathIndex].second == y) {
+            pathIndex++;
+            score++;
+        }
     }
 };
 
@@ -50,19 +125,34 @@ int main()
     char input;
     Character* character = Character::getInstance();
 
+    // Set the spawn and finish points
+    character->setSpawnPoint(0, 0);
+    character->setFinishPoint(10, 10);
+
     while (true) {
         // clear the console
         system("cls");
 
-        // display posisi karakter
-        for (int i = 0; i < character->getY(); i++) {
+        // display grid system
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i == character->getY() && j == character->getX()) {
+                    cout << "O ";
+                }
+                else {
+                    cout << ". ";
+                }
+            }
             cout << endl;
         }
-        for (int i = 0; i < character->getX(); i++) {
-            cout << " ";
+
+        cout << "posisi karakter: (" << character->getX() << ", " << character->getY() << ")" << endl;
+
+        // Check if the character has reached the finish point
+        if (character->hasReachedFinishPoint()) {
+            cout << "Selamat, Anda telah mencapai finish point!" << endl;
+            break;
         }
-        cout << "O";
-        cout << endl << "posisi karakter: (" << character->getX() << ", " << character->getY() << ")" << endl;
 
         // cek input
         cin >> input;
@@ -84,6 +174,9 @@ int main()
 
         _sleep(100);
     }
+
+    // Move the character
+    // ...
 
     return 0;
 }
